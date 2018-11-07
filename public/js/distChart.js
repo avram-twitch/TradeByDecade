53dda26a35df65735d824cfb4fbce41d31851cc9
avram-twitch <avram.twitch@gmail.com>
2018-11-07 14:45:25 -0700

    Fixed distributions to go from smallest to largest

--- public/js/distChart.js
+++ public/js/distChart.js
@@ -18,6 +18,14 @@ class DistributionChart {
         this.svgWidth = this.svgBounds.width - this.margin.left - this.margin.right;
         this.svgHeight = this.svgBounds.height;
 
+        this.groupPadding = 0.1;
+        this.groupHeight = (this.svgHeight - this.margin.top - this.margin.bottom) / this.allCodes.length;
+        this.groupWidth = (this.svgWidth - this.margin.left - this.margin.right) / 4;
+        this.groupMargin = {top: this.groupHeight * this.groupPadding, 
+                            right: this.groupWidth * this.groupPadding, 
+                            bottom:this.groupHeight * this.groupPadding, 
+                            left:this.groupWidth * this.groupPadding};
+
         this.svg = distChart.append("svg")
                             .attr("width", this.svgWidth)
                             .attr("height", this.svgHeight);
@@ -45,16 +53,29 @@ class DistributionChart {
 
         let allData = [];
 
+        this.codeScales = {}
+
         for (let i = 0; i < this.allCodes.length; i++)
         {
             let tmp = data.filter(function(d) {
                 return d.type == "export" && d.code == that.allCodes[i];
             });
-            tmp.sort(function(a, b) {
-                return d3.descending(a.countries.wld, b.countries.wld);
+
+            let max = d3.max(tmp, function(d) {
+                return +d.countries.wld;
             });
+
+            let scale = d3.scaleLinear()
+                          .domain([0, max])
+                          .range([this.groupMargin.bottom, this.groupHeight - this.groupMargin.bottom - this.groupMargin.top]);
+
+            tmp.sort(function(a, b) {
+                return d3.ascending(a.countries.wld, b.countries.wld);
+            });
+
             tmp = this.parse(tmp);
             allData.push(tmp);
+            this.codeScales[this.allCodes[i]] = scale;
         }
 
         let max = d3.max(data, function(d) {
@@ -63,18 +84,17 @@ class DistributionChart {
 
         console.log(allData);
 
-        let groupYScale = d3.scaleLinear()
-                            .domain([0, max])
-                            .range([0, (this.svgHeight - this.margin.bottom - this.margin.top) / 10]);
-
         let allYScale = d3.scaleLinear()
-                          .domain([0, 10])
+                          .domain([0, this.allCodes.length])
                           .range([this.margin.bottom, this.svgHeight - this.margin.bottom - this.margin.top])
 
         let area = d3.area()
-                     .x(function(d,i) { return i * (that.svgWidth / 250); })
+                     .x(function(d,i) { return that.groupMargin.left + (i * (that.groupWidth - that.groupMargin.right - that.groupMargin.left) / 250); })
                      .y0((d) => allYScale(d.code))
-                     .y1(function(d,i) { return allYScale(d.code) - groupYScale(d.val); });
+                     .y1(function(d,i) { 
+                         let base = allYScale(d.code);
+                         let height = that.codeScales[d.code](d.val);
+                         return base - height; });
 
         let groups = this.svg.selectAll("g")
                              .data(allData);
